2ec9ca67044f7c8bf46a6456aafc7ca10a29368f
Amirudheen Shareif <amirudheenshareif@Amirudheens-MacBook-Air.local>
2025-07-01 23:21:57 +0530

    padding-adjust

--- frontend/src/components/custom/Lexis.jsx
+++ frontend/src/components/custom/Lexis.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const Lexis = () => {
   return (
-   <div className='p-8 min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-800'>
+   <div className='p-5 min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-800'>
       <Header/>
       <Hero/>
      </div>
